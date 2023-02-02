06122c727af4f841187074550b6020bf29b29a09
Yu Gu <yugu772@yahoo.com>
2023-02-02 21:04:40 +0800

    Update run.py

--- source/run.py
+++ source/run.py
@@ -7,7 +7,4 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s - %(message)s
                     level=logging.INFO)
 
 if __name__ == "__main__":
-    # os.environ['TORCH_HOME'] = '/data/gu.826/.cahce'
-    # os.environ['ALLENNLP_CACHE_ROOT'] = '/data/gu.826/.cache'
-    # main(prog="python run.py")  # prog argument means nothing. Kun Tao wasn't aware of it.
     main()
